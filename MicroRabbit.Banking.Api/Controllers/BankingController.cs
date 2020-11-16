5b48f226098fc860ab99ce4c63927a499af8a62d
Cihan Bulut Akkurt <cbakkurt@outlook.com>
2020-11-16 15:30:02 +0300

    partialy completed  baning microservice

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountServices _accountServices;
+
+        public BankingController(IAccountServices accountServices)
+        {
+            _accountServices = accountServices;
+        }
+
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountServices.GetAccounts());
+        }
+
+      
+    }
+}
---
+++ MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+        public decimal AccountBalancer { get; set; }
+
+    }
+}
